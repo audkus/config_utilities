50d4de4f6dcb8e5232b53dbf0f701a38422ed621
rkw745 <steffen.rasmussen@adm.ku.dk>
2024-09-23 05:35:56 +0200

    Initial load

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     description='Utilities for configuration management and logging setup.',
     author='Steffen S. Rasmussen',
     author_email='steffen@audkus.dk',
-    url='http://yourprojecturl.com',
+    url='https://github.com/audkus/config_utilities.git',
 )
