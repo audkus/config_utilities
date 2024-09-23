15ef8adf3a8e65b26d765d44ff504583eafbc188
audkus <121483690+audkus@users.noreply.github.com>
2024-09-23 05:39:12 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='config_utilities',
+    version='0.2.0',
+    packages=find_packages(),
+    install_requires=[],
+    description='Utilities for configuration management and logging setup.',
+    author='Steffen S. Rasmussen',
+    author_email='steffen@audkus.dk',
+    url='https://github.com/audkus/config_utilities.git',
+)
