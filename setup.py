55df383a27bbf5030732fed22d8e6a4186ff5e2c
rkw745 <steffen.rasmussen@adm.ku.dk>
2024-09-25 15:41:05 +0200

    Changed name of app

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='config_utilities_audkus',
+    name='config_utilities',
     version='0.2.0',
     packages=find_packages(),
     install_requires=[],
